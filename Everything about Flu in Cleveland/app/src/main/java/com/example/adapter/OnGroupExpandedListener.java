90cfae85c57182123e99ea2bacf2443c50560ce2
zhenghao lu <blue@zhenghaos-MBP.attlocal.net>
2020-08-23 17:05:47 -0400

    commit

--- Everything about Flu in Cleveland/app/src/main/java/com/example/adapter/OnGroupExpandedListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.adapter;
-//from https://blog.csdn.net/shenggaofei/article/details/81870251
-public interface OnGroupExpandedListener {
-
-    void onGroupExpanded(int groupPosition);
-}
